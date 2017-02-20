52d1e70f1e9020996c889081ad2f5bb647fafb6e
MuJinYan <yanyan18202532973@163.com>
2017-02-20 21:55:48 +0800

    version 1.1

--- js/index.js
+++ js/index.js
@@ -196,6 +196,28 @@
             }
         } , { offset: '85%' } );
     };
+
+
+    var returnToTop = function () {
+        $(window).scroll(function(){
+            if ($(window).scrollTop()>100){
+                $("#back-to-top").fadeIn(1500);
+            }
+            else
+            {
+                $("#back-to-top").fadeOut(1500);
+            }
+        });
+
+        //当点击跳转链接后，回到页面顶部位置
+
+        $("#back-to-top").click(function(){
+            $('body,html').animate({scrollTop:0},1000);
+            return false;
+        });
+    };
+
+
     $(function(){
         fullHeight();
         sliderMain();
@@ -205,5 +227,6 @@
         burgerMenu();
         toggleBtnColor();
         contentWayPoint();
+        returnToTop();
     });
 }());
